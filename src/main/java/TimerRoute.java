9fdf67f3760959e1ef09c8d0c2ea2185d1e9d767
masanaka1234 <97520947+masanaka1234@users.noreply.github.com>
2025-12-04 22:57:53 +0900

    Refactor timer endpoint to use static builder

--- src/main/java/TimerRoute.java
+++ src/main/java/TimerRoute.java
@@ -1,10 +1,11 @@
 import org.apache.camel.builder.RouteBuilder;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.timer;
 
 public class TimerRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:foo?period=1000")
+        from(timer("foo").period(1000))
                 .log("Hello World");
     }
 }
