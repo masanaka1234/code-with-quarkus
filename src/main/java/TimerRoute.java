719f7d1c6b1d6a6f68dbba617e5cc85339b82341
masanaka1234 <97520947+masanaka1234@users.noreply.github.com>
2025-12-04 23:23:17 +0900

    Update timer endpoint syntax in TimerRoute

--- src/main/java/TimerRoute.java
+++ src/main/java/TimerRoute.java
@@ -1,11 +1,10 @@
 import org.apache.camel.builder.RouteBuilder;
-import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.timer;
 
 public class TimerRoute extends RouteBuilder {
 
-    @Override
-    public void configure() throws Exception {
-        from(timer("foo").period(1000))
-                .log("Hello World");
-    }
+   @Override
+   public void configure() throws Exception {
+       from("timer:foo?period=1000")
+               .log("Hello World");
+   }
 }
